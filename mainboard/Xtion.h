58c75fb6d3723e5514c2d9b7d812462e44c56d39
jordan.kubica@gmail.com <jordan.kubica@gmail.com@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-03-08 01:30:03 +0000

    placeholder for xtion object

---
+++ mainboard/Xtion.h
@@ -0,0 +1,32 @@
+// a class to manage data from Asus Xtion sensors collected through the OpenNI library.
+
+
+#ifndef XTION_H
+#define XTION_H
+
+#include "OpenNI stuff" // figure out specific dependencies
+
+
+class Xtion
+{
+
+        public:
+        
+        // constructor and destructor
+        
+        Xtion();
+        
+        ~Xtion(); //  gotta destroy dynamic data
+        
+        // various member functions and data structures
+        
+        
+        private:
+        
+        // other data?
+        
+}
+
+// other stuff goes here?
+
+#endif
