6c6f23194df5cae266383a1a24bda3975a736ec8
usstworkstation@gmail.com <usstworkstation@gmail.com@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-03-16 22:34:55 +0000

    Working on TwoWaySerialComm

---
+++ Java/src/StringContainer.java
@@ -0,0 +1,13 @@
+
+public class StringContainer {
+	private String str;
+	
+	public void setString(String s){
+		str = s;
+	}
+	
+	public String getString(){
+		return str;
+	}
+
+}
--- Java/src/TwoWaySerialComm.java
+++ Java/src/TwoWaySerialComm.java
@@ -14,6 +14,9 @@ public class TwoWaySerialComm
         super();
     }
     
+    private StringContainer received = new StringContainer();
+    private StringContainer toBeSent = new StringContainer();
+    
     void connect ( String portName ) throws Exception
     {
         CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
@@ -33,8 +36,8 @@ public class TwoWaySerialComm
                 InputStream in = serialPort.getInputStream();
                 OutputStream out = serialPort.getOutputStream();
                 
-                (new Thread(new SerialReader(in))).start();
-                (new Thread(new SerialWriter(out))).start();
+                (new Thread(new SerialReader(in, received))).start();
+                (new Thread(new SerialWriter(out, toBeSent))).start();
 
             }
             else
@@ -48,10 +51,12 @@ public class TwoWaySerialComm
     public static class SerialReader implements Runnable 
     {
         InputStream in;
+        StringContainer out;
         
-        public SerialReader ( InputStream in )
+        public SerialReader ( InputStream in , StringContainer out)
         {
             this.in = in;
+            this.out = out;
         }
         
         public void run ()
@@ -60,9 +65,13 @@ public class TwoWaySerialComm
             int len = -1;
             try
             {
+            	System.out.println("Not end of file yet");
                 while ( ( len = this.in.read(buffer)) > -1 )
                 {
-                    System.out.print(new String(buffer,0,len));
+                	//System.out.println("Not end of file yet");
+           
+                    //out.setString(out.getString() + new String(buffer,0,len));
+                	System.out.print(new String(buffer,0,len));
                 }
             }
             catch ( IOException e )
@@ -76,10 +85,12 @@ public class TwoWaySerialComm
     public static class SerialWriter implements Runnable 
     {
         OutputStream out;
+        StringContainer in;
         
-        public SerialWriter ( OutputStream out )
+        public SerialWriter ( OutputStream out, StringContainer in)
         {
             this.out = out;
+            this.in = in;
         }
         
         public void run ()
@@ -87,10 +98,21 @@ public class TwoWaySerialComm
             try
             {                
                 int c = 0;
-                while ( ( c = System.in.read()) > -1 )
+                /*while ( ( c = System.in.read()) > -1 )
                 {
                     this.out.write(c);
-                }                
+                }     */
+                
+            	while (true){
+            		String str = in.getString();
+            		if (str.length() != 0){
+            			// write the first char
+            			this.out.write(str.charAt(0));
+            			System.out.println("Printed: " + str.charAt(0));
+            			// keep the remainder of the string
+            			in.setString(str.substring(1));
+            		}
+            	}
             }
             catch ( IOException e )
             {
@@ -98,6 +120,19 @@ public class TwoWaySerialComm
             }            
         }
     }
+    
+    public String readMessage(){
+    	String str = received.getString();    	
+    	received.setString(null);
+    	return str;    	
+    }
+    
+    public void writeMessage(String msg){
+    	toBeSent.setString(msg);
+    }
+    
+    
+    
     
     public static void main ( String[] args )
     {
@@ -110,6 +145,16 @@ public class TwoWaySerialComm
         catch ( Exception e ) {    
             e.printStackTrace();
         }
+        
+        port.writeMessage("Hi\n");
+        
+        /*try {
+			//Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+        System.out.println(port.readMessage());
         
         
     }
