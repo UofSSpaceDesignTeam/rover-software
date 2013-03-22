bee75996f575947f537a23e19f78ca17ed88e6c5
usstworkstation <usstworkstation@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-03-22 02:25:23 +0000

    Wrote classes "Instrument" and "Actuator" based on new architecture:
    Instrument is the superclass of both sensor and actuator, and encapsulates methods/fields that are required by both. (Arduino field, idByte 1 field, setEnabled method)
    
    Sensor and Actuator have Instrument as a superclass, and also implement their own methods.
    Sensor -> addData, setDataTransferRate
    Actuator -> set
    Sensors

---
+++ Java/src/prototype1/Actuator.java
@@ -0,0 +1,26 @@
+package prototype1;
+
+import arduino.Arduino;
+
+public abstract class Actuator extends Instrument
+{
+	/**
+	 * Constructor that calls the super constructor (Instrument)
+	 * @param attachedArduino	The arduino that this instrument is physically attached to
+	 * @param thisId	the [ID1] byte that represents this instrument (M for motor; I for IMU, etc)
+	 */
+	Actuator(Arduino attachedArduino, byte thisId)
+	{
+		super(attachedArduino, thisId);
+	}
+	
+	/**
+	 * The method to set data to an actuator
+	 * @param data	The data bytes to set; [B1]...[Bn]
+	 */
+	protected void set(byte[] data)
+	{
+		this.sendToArduino(MessageProtocol.ID2_SET, data);
+	}
+	
+}
---
+++ Java/src/prototype1/Instrument.java
@@ -0,0 +1,72 @@
+package prototype1;
+
+import arduino.Arduino;
+
+public abstract class Instrument 
+{
+	//Store the ID of the sensor/actuator
+	protected byte id;
+	
+	//Store the Arduino that this instrument is attached to
+	protected Arduino associatedArduino;
+	
+	/**
+	 * Constructor for this class
+	 * @param attachedArduino	The arduino that this instrument is physically attached to
+	 * @param thisId	the [ID1] byte that represents this instrument (M for motor; I for IMU, etc)
+	 */
+	Instrument(Arduino attachedArduino, byte thisId)
+	{
+		id = thisId;
+		associatedArduino = attachedArduino;
+	}
+	
+	/**
+	 * The method to send data to the arduino in the proper format
+	 * @param id2	The [ID2] byte to send, representing the command
+	 * @param data	The data array to send, representing [B1]...[Bn], where n is the length of the data sent
+	 */
+	protected void sendToArduino(byte id2, byte[] data)
+	{
+		int length = data.length;
+		
+		//Fill in the general data stream using the data passed in
+		// #[ID1][ID2][DL1][DL2][B1]...[Bn][\n]
+		byte enableMessage[] = new byte[length+6];
+		
+		enableMessage[0] = '#';
+		enableMessage[1] = this.id;
+		enableMessage[2] = id2;
+		enableMessage[3] = (byte) (length/256);
+		enableMessage[4] = (byte) (length%256);
+		
+		int i = 0;
+		for(i = 0; i < length; i++)
+		{
+			enableMessage[i+5] = data[i];
+		}
+		enableMessage[i] = '\n';
+		
+		associatedArduino.sendMessage(enableMessage);
+	}
+	
+	/**
+	 * Sets the instrument to be enabled or disabled
+	 * @param enable	true to enable instrument, false to disable
+	 */
+	public void setEnabled(boolean enable)
+	{
+		//Fill in the data[] to send to the Arduino
+		byte toSend[] = new byte[1];
+		if(enable)
+		{
+			toSend[0] = '1';
+		}
+		else
+		{
+			toSend[0] = '0';
+		}
+		this.sendToArduino(MessageProtocol.ID2_ENABLE_DISABLE, toSend);
+	}
+	
+}
