cd53dbcbcc2c0bba77d3d899b80431439250d6a3
usstworkstation@gmail.com <usstworkstation@gmail.com@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-03-23 17:52:34 +0000

    Added methods to turn on LED, turn off LED, and start LED blinking in LED class

--- Java/src/prototype1/Led.java
+++ Java/src/prototype1/Led.java
@@ -2,16 +2,44 @@ package prototype1;
 
 public class Led extends Actuator
 {
+
+	
+	private byte[] turnOnCall {'1'};
+	private byte[] turnOffCall = {'0'};
+	private byte[] blinkCall = {'b'};
+	
 	
 	/**
 	 * Constructor that calls the super constructor (Instrument)
 	 * @param attachedArduino	The arduino that this instrument is physically attached to
-	 * @param thisId	the [ID1] byte that represents this instrument (M for motor; I for IMU, etc)
 	 */
-	public Led(Arduino attachedArduino, byte thisId)
-	{
-		super(attachedArduino, thisId);
+	public Led(Arduino attachedArduino)
+	{	
+		super(attachedArduino, ID1_LED);
 	}
 	
+	/**
+	 * Turns the Led on for the Arduino
+	 */
+	public turnOn()
+	{
+		super.set(turnOnCall);
+	}
+	
+	/**
+	 * Turns off the Led
+	 */
+	public turnOff()
+	{
+		super.set(turnOffCall);
+	}
+	
+	/**
+	 * Start the Led blinking
+	 */
+	public startBlinking()
+	{
+		super.set(blinkCall);
+	}
 	
 }
