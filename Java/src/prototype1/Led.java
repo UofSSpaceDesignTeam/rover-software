e7f9399dafc25d042a00eba309413daa3122dccc
usstworkstation@gmail.com <usstworkstation@gmail.com@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-03-23 17:07:13 +0000

    modified Led

--- Java/src/prototype1/Led.java
+++ Java/src/prototype1/Led.java
@@ -1,5 +1,17 @@
 package prototype1;
 
-public class Led {
-
+public class Led extends Actuator
+{
+	
+	/**
+	 * Constructor that calls the super constructor (Instrument)
+	 * @param attachedArduino	The arduino that this instrument is physically attached to
+	 * @param thisId	the [ID1] byte that represents this instrument (M for motor; I for IMU, etc)
+	 */
+	public Led(Arduino attachedArduino, byte thisId)
+	{
+		super(attachedArduino, thisId);
+	}
+	
+	
 }
