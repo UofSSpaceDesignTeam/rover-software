d1ad9f05eb825b8a4d80cdb9d9419b2d72181f1c
usstworkstation@gmail.com <usstworkstation@gmail.com@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-07-06 18:10:51 +0000

    added camera classes

---
+++ Java/src/instruments/Camera.java
@@ -0,0 +1,25 @@
+package instruments;
+
+import environment.Situation;
+
+public class Camera
+{
+	Situation situation;
+
+	/**
+	 * @return the situation
+	 */
+	public Situation getSituation()
+	{
+		return situation;
+	}
+
+	/**
+	 * @param situation the situation to set
+	 */
+	public void setSituation(Situation situation)
+	{
+		this.situation = situation;
+	}
+
+}
---
+++ Java/src/messaging/CameraMessageHandler.java
@@ -0,0 +1,52 @@
+package messaging;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class CameraMessageHandler
+{
+	
+	private Process cameraProcess;
+	private BufferedReader cameraOut;
+	private BufferedWriter cameraIn;
+	
+	public CameraMessageHandler(){
+		ProcessBuilder pb = new ProcessBuilder("/home/fit-pc/workspace/openni_grabber-Debug@RoverPCL/jsonTest");
+		
+		try
+		{
+			cameraProcess = pb.start();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		InputStream instream = cameraProcess.getInputStream();		
+		OutputStream outstream = cameraProcess.getOutputStream();
+		
+		cameraOut = new BufferedReader(new InputStreamReader(instream));
+		cameraIn = new BufferedWriter(new OutputStreamWriter(outstream));
+		
+		
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		CameraMessageHandler cmh = new CameraMessageHandler();
+		cmh.cameraIn.write("bla\n");
+		cmh.cameraIn.flush();
+		System.out.println(cmh.cameraOut.readLine());
+
+	}
+
+}
