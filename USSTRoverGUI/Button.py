fb1359b5b55f6dd6e55951f875cf57f4917a6d0d
usstworkstation <usstworkstation@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2013-11-15 00:43:12 +0000

    added the communication python code and button python code

---
+++ USSTRoverGUI/Button.py
@@ -0,0 +1,23 @@
+import pygame
+pygame.init()
+
+class Button:
+   def __init__(self, text):
+      self.text = text
+      self.default_color = (100,100,100)
+      self.font_color = (0,0,0)
+      self.obj = None
+      
+   def label(self):
+      '''button label font'''
+      font = pygame.font.Font(None, 20)
+      return font.render(self.text, 1, self.font_color)
+      
+   def color(self):
+      '''change color when hovering'''
+      return self.default_color
+         
+   def draw(self, screen, mouse, rectcoord, labelcoord):
+      '''create rect obj, draw, and change color based on input'''
+      self.obj  = pygame.draw.rect(screen, self.color(), rectcoord)
+      screen.blit(self.label(), labelcoord)
---
+++ USSTRoverGUI/communication.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import socket
+import commands
+import os
+import signal
+import time
+import subprocess
+import Button
+import pygame, random
+from pygame.locals import *
+
+
+screen = pygame.display.set_mode((0,0),pygame.RESIZABLE)
+#cmd = ['nc', '-lu', '-p', 3001]|['mplayer', '-geometry', '50%:30', '-x', 850, '-y', 450, '-nosound', '-nolirc', '-noborder', '-hardframedrop', '-noautosub', '-fps', 35, '-demuxer', 'h264es', '-nocache']
+#screen = pygame.display.set_mode([1000,1000])
+
+pygame.init()
+
+def connect(port , x, y, frameps):
+  global p
+  command = "nc -lu -p 3001 | mplayer -geometry 50%:30 -x 850 -y 450 -nosound -nolirc -noborder -hardframedrop -noautosub -fps frameps -demuxer h264es -nocache -"
+  p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+#	os.system(command)
+#
+# This is where the main loop starts
+#
+
+btn = Button.Button('Connect')
+btn2 = Button.Button('Disconect')
+btn3 = Button.Button('Button 3')
+p = 0
+screen.fill([255,255,255])
+ 
+mainloop, x, y, color, fontsize, delta, fps =  True, 25 , 0, (32,32,32), 35, 1, 30
+ 
+Clock = pygame.time.Clock()
+ 
+while mainloop:
+   tickFPS = Clock.tick(fps)
+   frameps = 35
+   x = 850
+   y = 450
+   port = 3001
+   mouse = pygame.mouse.get_pos()
+   pygame.display.set_caption("Press Esc to quit. FPS: %.2f" % (Clock.get_fps()))
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           mainloop = False # Be IDLE friendly!
+       elif event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_ESCAPE:
+               mainloop = False # Be IDLE friendly!
+       elif event.type == pygame.MOUSEBUTTONDOWN:
+           if btn.obj.collidepoint(mouse):
+              connect()
+           elif btn2.obj.collidepoint(mouse):
+              os.kill(p.pid, signal.SIGINT)           
+           elif btn3.obj.collidepoint(mouse):
+              print('button 3 clicked')
+              
+   btn.draw(screen, mouse, (100,100,100,20), (125,103))
+   btn2.draw(screen, mouse, (100,130,100,20), (125,133))
+   btn3.draw(screen, mouse, (100,160,100,20), (125,163))
+   pygame.display.update()
+ 
+pygame.quit()
